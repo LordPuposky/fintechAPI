e4d863bf7f3a685ee6eca80647d6d89d37acc90d
Yesid Romero <augustomero@gmail.com>
2026-01-24 12:58:46 -0500

    Fix: Group get transaction by ID under Transactions tag

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -41,15 +41,18 @@ const createTransaction = async (req, res) => {
     }
 };
 
-// Get a transaction by ID
-const getTransactionById = async (req, res) => {
+// Get a single transaction by ID
+const getSingle = async (req, res) => {
+    // #swagger.tags = ['Transactions']
     try {
         const transactionId = req.params.id;
         const transaction = await Transaction.findById(transactionId);
         if (!transaction) {
-            return res.status(404).json({ message: 'Transaction not found' });
+            res.status(404).json({ message: 'Transaction not found' });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(transaction);
         }
-        res.status(200).json(transaction);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
