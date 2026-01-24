549252a91992e0bc9a1f5a1da2e75d56f3fd1107
Yesid Romero <augustomero@gmail.com>
2026-01-24 10:55:42 -0500

    Add swagger body to updateTransantion

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -41,9 +41,36 @@ const createTransaction = async (req, res) => {
     }
 };
 
+// Get a transaction by ID
+const getTransactionById = async (req, res) => {
+    try {
+        const transactionId = req.params.id;
+        const transaction = await Transaction.findById(transactionId);
+        if (!transaction) {
+            return res.status(404).json({ message: 'Transaction not found' });
+        }
+        res.status(200).json(transaction);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
 // Update a transaction
 const updateTransaction = async (req, res) => {
-    /* #swagger.tags = ['Transactions'] */
+    /* #swagger.tags = ['Transactions']
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'Transaction details to update',
+            schema: {
+                accountId: '69740833...',
+                type: 'Deposit',
+                amount: 5000,
+                date: '2026-01-23',
+                description: 'Updated description',
+                status: 'Completed'
+            }
+        }
+    */
     try {
         const transactionId = req.params.id;
         const response = await Transaction.findByIdAndUpdate(transactionId, req.body, { new: true, runValidators: true });
@@ -78,19 +105,7 @@ const deleteTransaction = async (req, res) => {
     }
 };
 
-// Get a transaction by ID
-const getTransactionById = async (req, res) => {
-    try {
-        const transactionId = req.params.id;
-        const transaction = await Transaction.findById(transactionId);
-        if (!transaction) {
-            return res.status(404).json({ message: 'Transaction not found' });
-        }
-        res.status(200).json(transaction);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
+
 
 
 
